e69e8a0954359cdb79679146522a97a21d7a4875
Matheus <matheus.reis13@hotmail.com>
2019-11-03 20:39:09 -0300

    Aplicação pronta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public mode = 'list';
   public todos: Todo[] = [];
   public title: String = 'Minhas tarefas';
   public form: FormGroup;
@@ -20,27 +21,58 @@ export class AppComponent {
         Validators.required
       ])]
     });
-    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2,'Ir ao supermercado', false));
-    this.todos.push(new Todo(3,'Estudar angular', true));
-  
+    this.load();
     
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id= this.todos.length +1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
   }
 
   remove (todo: Todo) {
     const index = this.todos.indexOf(todo);
     if(index !== -1) {
       this.todos.splice(index, 1);
+      this.save();
     }
 
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
 
   }
  
   markAsUndone (todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.mode = 'list';
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if(data){
+      this.todos = JSON.parse(data);
+    }else{
+      this.todos = [];
+    }
+    
+  }
+  changeMode(mode: string){
+    this.mode = mode;
   }
 }
